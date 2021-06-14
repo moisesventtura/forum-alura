f3f65921d13f763bec179a36392f58727e190351
moisesventtura <moisesventura15@gmail.com>
2021-06-14 15:49:54 -0300

    Topicos Controller criada

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,2 +1,24 @@
-package br.com.alura.forum.controller;public class TopicosController {
+package br.com.alura.forum.controller;
+
+
+import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class TopicosController {
+
+    @RequestMapping("/topicos")
+    public List<TopicoDto> lista(){
+        Topico topico = new Topico("Dúvida", "Duvida Com Spring", new Curso("Spring ", "Programação"));
+
+        return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+    }
 }
