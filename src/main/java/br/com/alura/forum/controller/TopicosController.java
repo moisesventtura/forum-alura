9c8e831c1f824f545f9d63a5d13bf40e37884bb7
moisesventtura <moisesventura15@gmail.com>
2021-06-17 10:14:35 -0300

    ResponseEntity criado para retornar 201 informando que foi processado com sucesso

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,15 +1,18 @@
 package br.com.alura.forum.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.repository.CursoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.TopicoRepository;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/topicos")
@@ -33,9 +36,13 @@ public class TopicosController {
 	}
 
 	@PostMapping
-	public void cadastrar(@RequestBody TopicoForm form){
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder){
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
+
+		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+
+		return ResponseEntity.created(uri).body(new TopicoDto(topico));
 	}
 
 }
