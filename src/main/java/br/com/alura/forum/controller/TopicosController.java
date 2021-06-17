21af54da26b40e62d3483cef7b4f56abff622f70
moisesventtura <moisesventura15@gmail.com>
2021-06-17 10:02:23 -0300

    Método para cadastrar tópicos criado

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -2,21 +2,26 @@ package br.com.alura.forum.controller;
 
 import java.util.List;
 
+import br.com.alura.forum.controller.form.TopicoForm;
+import br.com.alura.forum.repository.CursoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
+@RequestMapping("/topicos")
 public class TopicosController {
 	
 	@Autowired
 	private TopicoRepository topicoRepository;
+
+	@Autowired
+	private CursoRepository cursoRepository;
 	
-	@RequestMapping("/topicos")
+	@GetMapping
 	public List<TopicoDto> lista(String nomeCurso) {
 		if (nomeCurso == null) {
 			List<Topico> topicos = topicoRepository.findAll();
@@ -25,6 +30,12 @@ public class TopicosController {
 			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
 			return TopicoDto.converter(topicos);
 		}
+	}
+
+	@PostMapping
+	public void cadastrar(@RequestBody TopicoForm form){
+		Topico topico = form.converter(cursoRepository);
+		topicoRepository.save(topico);
 	}
 
 }
