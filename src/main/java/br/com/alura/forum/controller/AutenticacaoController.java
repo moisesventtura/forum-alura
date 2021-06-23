a4e5e2539c8912f7e7fde233ce40b6a31994fc1d
moisesventtura <moisesventura15@gmail.com>
2021-06-22 21:37:01 -0300

    Gerando token com JWT

--- src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
+++ src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
@@ -1,7 +1,14 @@
 package br.com.alura.forum.controller;
 
+import br.com.alura.forum.config.security.TokenService;
+import br.com.alura.forum.controller.dto.TokenDto;
 import br.com.alura.forum.controller.form.LoginForm;
+import org.h2.security.auth.AuthenticationException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,11 +20,21 @@ import javax.validation.Valid;
 @RequestMapping("/auth")
 public class AutenticacaoController {
 
-    @PostMapping
-    public ResponseEntity<?> autenticar(@RequestBody @Valid LoginForm form){
-        System.out.println(form.getEmail());
-        System.out.println(form.getSenha());
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
-        return ResponseEntity.ok().build();
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping
+    public ResponseEntity<TokenDto> autenticar(@RequestBody @Valid LoginForm form){
+        UsernamePasswordAuthenticationToken dadosLogin = form.converter();
+
+        Authentication authentication = authenticationManager.authenticate(dadosLogin);
+        String token = tokenService.gerarToken(authentication);
+        System.out.println(token);
+        return ResponseEntity.ok(new TokenDto(token, "Bearer"));
+
+
     }
 }
