3fdf46d780b87ced75a7b32ea53d0d4e64b54feb
moisesventtura <moisesventura15@gmail.com>
2021-07-03 15:55:51 -0300

    Testando um Controller

--- src/main/java/br/com/alura/forum/config/swagger/SwaggerConfigurations.java
+++ src/main/java/br/com/alura/forum/config/swagger/SwaggerConfigurations.java
@@ -1,8 +1,11 @@
 package br.com.alura.forum.config.swagger;
 
-import br.com.alura.forum.modelo.Usuario;
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import br.com.alura.forum.modelo.Usuario;
 import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -10,30 +13,25 @@ import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
-import java.util.Arrays;
-
 @Configuration
 public class SwaggerConfigurations {
+	
+	@Bean
+	public Docket forumApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("br.com.alura.forum"))
+				.paths(PathSelectors.ant("/**"))
+				.build()
+				.ignoredParameterTypes(Usuario.class)
+				.globalOperationParameters(Arrays.asList(
+						new ParameterBuilder()
+						.name("Authorization")
+						.description("Header para token JWT")
+						.modelRef(new ModelRef("string"))
+						.parameterType("header")
+						.required(false)
+						.build()));
+	}
 
-    @Bean
-    public Docket forumApi(){
-        return new Docket(DocumentationType.SWAGGER_2) //Tipo da Documentação
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("br.com.alura.forum")) //A partir de qual pacote vai começa a ler
-                .paths(PathSelectors.ant("/**")) // quais endpoints fazer analise
-                .build()
-                .ignoredParameterTypes(Usuario.class) // Ignore todas as URLs que trabalham com a classe Usuario, ignorar objetos do tipo Usuario
-                .globalOperationParameters( // Adicionar parametros globais
-                        Arrays.asList( // recebe a lista com os parametros
-                                new ParameterBuilder()
-                                        .name("Authorization")
-                                        .description("Header para Token JWT")
-                                        .modelRef(new ModelRef("string"))
-                                        .parameterType("header")
-                                        .required(false)
-                                        .build()));
-
-
-        // para acessar localhost:8080/swagger-ui.html
-    }
 }
--- src/main/java/br/com/alura/forum/repository/CursoRepository.java
+++ src/main/java/br/com/alura/forum/repository/CursoRepository.java
@@ -6,7 +6,6 @@ import br.com.alura.forum.modelo.Curso;
 
 public interface CursoRepository extends JpaRepository<Curso, Long> {
 
-
 	Curso findByNome(String nome);
 
 }
